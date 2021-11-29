3a1dbdc0a1406e518d9eae5fb7b2ccf48c2ec4d6
Mark Goadrich <goadrich@hendrix.edu>
2021-11-29 15:49:19 -0600

    Seeing if C# is right for this year

---
+++ 2021/AoCSharp/AoCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");
+        }
+    }
+}
