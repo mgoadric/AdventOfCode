50ef19b57a2e0ef1bbde188f8524b3288ea71600
Mark Goadrich <goadrich@hendrix.edu>
2021-12-02 10:36:18 -0600

    Day 2 in C#, added more structure

--- 2021/AoCSharp/AoCSharp/Program.cs
+++ 2021/AoCSharp/AoCSharp/Program.cs
@@ -7,21 +7,32 @@ namespace AoCSharp
 {
     class Program
     {
+
+        delegate int Puzzle(IEnumerable<String> input);
+
         static void Main(string[] args)
         {
             // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-a-text-file-one-line-at-a-time
 
             string path = "/Users/goadrich/Github/AdventOfCode/2021/data/";
 
-            Console.WriteLine("1.1 sample: " +
-                day1part1(File.ReadLines(path + "input1sample.txt")));
-            Console.WriteLine("1.1 full: " +
-                day1part1(File.ReadLines(path + "input1.txt")));
 
-            Console.WriteLine("1.2 sample: " +
-                day1part2(File.ReadLines(path + "input1sample.txt")));
-            Console.WriteLine("1.2 full: " +
-                day1part2(File.ReadLines(path + "input1.txt")));
+            Puzzle[,] puzzles = new Puzzle[,]{
+                { day1part1, day1part2 },
+                { day2part1, day2part2 },
+            };
+
+            for (int day = 1; day < (puzzles.Length / 2) + 1; day++)
+            {
+                for (int part = 1; part < 3; part++)
+                {
+                    Console.WriteLine(String.Format("Day {0,2} Part {1}", day, part));
+                    Console.WriteLine(" sample: " +
+                                  puzzles[day - 1, part - 1](File.ReadLines(path + String.Format("input{0}sample.txt", day))));
+                    Console.WriteLine(" full: " +
+                                  puzzles[day - 1, part - 1](File.ReadLines(path + String.Format("input{0}.txt", day))));
+                }
+            }
         }
 
         static int day1part1(IEnumerable<string> input)
@@ -50,7 +61,8 @@ namespace AoCSharp
             {
                 int reading = Int32.Parse(line);
                 q.Enqueue(reading);
-                if (q.Count > window) {
+                if (q.Count > window)
+                {
                     int total = q.Sum();
                     int r = q.Dequeue();
                     if (total - r > total - reading)
@@ -60,6 +72,64 @@ namespace AoCSharp
                 }
             }
             return count;
+        }
+
+        static int day2part1(IEnumerable<string> input)
+        {
+            int h = 0;
+            int d = 0;
+            foreach (string line in input)
+            {
+                String[] commands = line.Split(" ");
+                switch (commands[0])
+                {
+                    case "forward":
+                        h += Int32.Parse(commands[1]);
+                        break;
+                    case "up":
+                        d -= Int32.Parse(commands[1]);
+                        break;
+                    case "down":
+                        d += Int32.Parse(commands[1]);
+                        break;
+                }
+            }
+            return h * d;
+        }
+
+        static int day2part2(IEnumerable<string> input)
+        {
+            int h = 0;
+            int d = 0;
+            int aim = 0;
+            foreach (string line in input)
+            {
+                String[] commands = line.Split(" ");
+                switch (commands[0])
+                {
+                    case "forward":
+                        h += Int32.Parse(commands[1]);
+                        d += aim * Int32.Parse(commands[1]);
+                        break;
+                    case "up":
+                        aim -= Int32.Parse(commands[1]);
+                        break;
+                    case "down":
+                        aim += Int32.Parse(commands[1]);
+                        break;
+                }
+            }
+            return h * d;
+        }
+
+        static int day3part1(IEnumerable<string> input)
+        {
+            return 0;
+        }
+
+        static int day3part2(IEnumerable<string> input)
+        {
+            return 0;
         }
     }
 }
