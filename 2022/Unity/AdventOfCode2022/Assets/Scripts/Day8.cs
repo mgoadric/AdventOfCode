c9c0d60977e36841cf2a8ad269fa16b7b8ffce99
Mark Goadrich <goadrich@hendrix.edu>
2022-12-09 11:03:33 -0600

    Part 2 is complete!

--- 2022/Unity/AdventOfCode2022/Assets/Scripts/Day8.cs
+++ 2022/Unity/AdventOfCode2022/Assets/Scripts/Day8.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -106,6 +107,48 @@ public class Day8 : MonoBehaviour
 
         print("Part 1: " + visibleCount);
 
+        Tuple<int, int>[] dirs = {
+            new Tuple<int, int>(0, 1),
+            new Tuple<int, int>(1, 0),
+            new Tuple<int, int>(0, -1),
+            new Tuple<int, int>(-1, 0),
+        };
+
+        int bestScenic = 0;
+        for (int i = 1; i < data.Count() - 1; i++) {
+            for (int j = 1; j < data[i].Count() - 1; j++) {
+                
+                int scenicScore = 1;
+                foreach(Tuple<int, int> d in dirs) {
+                    int x = i;
+                    int y = j;
+                    int distance = 1;
+                    bool looking = true;
+                    do {
+                        x += d.Item1;
+                        y += d.Item2;
+                        //print("comparing " + data[x][y] + " to " + data[i][j]);
+                        if (x == 0 || x == data.Count() - 1 || y == 0 || y == data.Count() - 1) {
+                            //print("edge, stopping");
+                            looking = false;
+                        } else if (data[x][y] < data[i][j]) {
+                            //print("less");
+                            distance++;
+                        } else if (data[x][y] >= data[i][j]) {
+                            //print("stopping");
+                            looking = false;
+                        }
+                    } while (looking);
+                    scenicScore *= distance;
+                }
+                if (scenicScore > bestScenic) {
+                    bestScenic = scenicScore;
+                    //print("best yet at " + i + ", " + j + ": " + bestScenic);
+                }
+            }
+        }
+
+        print("Part 2:" + bestScenic);
 
     }
 
