e1ec028134d27b3d3ae17902720ad50c11badc74
Mark Goadrich <goadrich@hendrix.edu>
2022-12-17 13:03:13 -0600

    More Terrain progress

--- 2022/Unity/AdventOfCode2022/Assets/Scripts/Day12.cs
+++ 2022/Unity/AdventOfCode2022/Assets/Scripts/Day12.cs
@@ -8,6 +8,9 @@ public class Day12 : MonoBehaviour
 {
 
     public GameObject terrain;
+    public GameObject start;
+
+    public GameObject end;
 
     private float[,] worldmap;
 
@@ -24,7 +27,16 @@ public class Day12 : MonoBehaviour
         for (int i = 0; i < data.Count; i++) {
             char[] letters = data[i].ToCharArray();
             for (int j = 0; j < letters.Length; j++) {
-                //worldmap[i, j] = (letters[j] - 'a') / 26.0f;
+                if (letters[j] == 'S') {
+                    // Make start here
+                    start.transform.position = new Vector3(100 + i, 0, 100 + j);
+                    letters[j] = 'a';
+                } else if (letters[j] == 'E') {
+                    // Make end here
+                    end.transform.position = new Vector3(100 + i, 0, 100 + j);
+                    letters[j] = 'z';
+                }
+                worldmap[i, j] = (2 + (letters[j] - 'a')) / 280.0f;
             }
         }
     }
@@ -35,7 +47,7 @@ public class Day12 : MonoBehaviour
         string path = "Assets/Data/input12.txt";
         LoadData(path);
         Terrain ter = terrain.GetComponent<Terrain>();
-        ter.terrainData.SetHeights(30, 30, worldmap);
+        ter.terrainData.SetHeights(100, 100, worldmap);
     }
 
     // Update is called once per frame
