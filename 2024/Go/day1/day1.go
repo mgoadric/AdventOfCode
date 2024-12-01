246648ddbe39f0837e64dd8ee07db5d223d6cacb
Mark Goadrich <goadrich@hendrix.edu>
2024-12-01 13:16:21 -0600

    completed part 2

--- 2024/Go/day1/day1.go
+++ 2024/Go/day1/day1.go
@@ -13,9 +13,8 @@ func check(e error) {
 	}
 }
 
-func part1() int {
-
-	dat, err := os.ReadFile("../day/1/input.txt")
+func parsing() ([]int, []int) {
+	dat, err := os.ReadFile("../../day/1/input.txt")
 	check(err)
 
 	a := make([]int, 0)
@@ -31,13 +30,18 @@ func part1() int {
 		b = append(b, second)
 	}
 
-	slices.Sort(a)
-	slices.Sort(b)
+	return a, b
+}
+
+func part1() int {
+	left, right := parsing()
+	slices.Sort(left)
+	slices.Sort(right)
 
 	total := 0
 
-	for i := range len(a) {
-		x := a[i] - b[i]
+	for i := range len(left) {
+		x := left[i] - right[i]
 		if x < 0 {
 			x = -x
 		}
@@ -46,6 +50,27 @@ func part1() int {
 	return total
 }
 
+func part2() int {
+
+	left, right := parsing()
+
+	m := make(map[int]int)
+	n := make(map[int]int)
+
+	for i := range len(left) {
+		m[left[i]] += 1
+		n[right[i]] += 1
+	}
+
+	total := 0
+	for k, v := range m {
+		total += k * v * n[k]
+	}
+
+	return total
+}
+
 func main() {
 	fmt.Println("Part 1 answer: " + strconv.Itoa(part1()))
+	fmt.Println("Part 2 answer: " + strconv.Itoa(part2()))
 }
