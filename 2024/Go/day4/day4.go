a9fb380bc2b6f4878e8a34fef13b816249d992e6
Mark Goadrich <goadrich@hendrix.edu>
2024-12-04 09:36:59 -0600

    Day 4 complete

--- 2024/Go/day4/day4.go
+++ 2024/Go/day4/day4.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -12,30 +13,107 @@ func check(e error) {
 	}
 }
 
-func parsing() []byte {
+func parsing() []string {
 	dat, err := os.ReadFile("../../day/4/input.txt")
 	check(err)
 
-	return dat
+	return strings.Split(string(dat), "\n")
+}
+
+type point struct {
+	x int
+	y int
 }
 
 func part1() int {
-	//data := parsing()
+	data := parsing()
+
+	size := 140
+	dirs := []point{
+		{x: 1, y: 0},
+		{x: -1, y: 0},
+		{x: 0, y: 1},
+		{x: 0, y: -1},
+		{x: 1, y: 1},
+		{x: -1, y: 1},
+		{x: 1, y: -1},
+		{x: -1, y: -1},
+	}
+
+	target := "XMAS"
 
 	total := 0
-	for {
-		break
+	for i := range size {
+		for j := range size {
+			if data[i][j] == 'X' {
+				for _, d := range dirs {
+					spot := point{x: i, y: j}
+					found := 1
+					for {
+						if found == len(target) {
+							break
+						}
+						spot.x += d.x
+						spot.y += d.y
+						if spot.x >= size || spot.y >= size || spot.x < 0 || spot.y < 0 {
+							break
+						}
+						if data[spot.x][spot.y] != target[found] {
+							break
+						}
+						found++
+					}
+					if found == len(target) {
+						total += 1
+					}
+				}
+			}
+		}
 	}
 
 	return total
 }
 
 func part2() int {
-	//data := parsing()
+	data := parsing()
+
+	size := 140
+	dirs := []point{
+		{x: 1, y: 1},
+		{x: -1, y: 1},
+		{x: -1, y: -1},
+		{x: 1, y: -1},
+	}
+
+	target := "MMSS"
 
 	total := 0
-	for {
-		break
+	for i := range size {
+		for j := range size {
+			if data[i][j] == 'A' {
+				for n := range dirs {
+					found := 0
+					for {
+						if found == len(target) {
+							break
+						}
+						spot := point{x: i + dirs[n].x, y: j + dirs[n].y}
+						if spot.x >= size || spot.y >= size || spot.x < 0 || spot.y < 0 {
+							break
+						}
+						if data[spot.x][spot.y] != target[found] {
+							break
+						}
+						found++
+						n++
+						n %= len(dirs)
+					}
+					if found == len(target) {
+						total += 1
+					}
+				}
+			}
+		}
 	}
 
 	return total
